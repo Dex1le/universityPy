6d2a7cab8a8e555b7ba4287ea47fc0260b50fe8e
Dex1le <119615676+Dex1le@users.noreply.github.com>
2023-12-23 13:01:10 +0300

    first commit

---
+++ 23.12.2023/spiski.py
@@ -0,0 +1,27 @@
+def split_list(nums):
+    total_sum = sum(nums)
+    if total_sum % 2 != 0:
+        return False
+
+    target_sum = total_sum // 2
+    current_sum = 0
+    first_list = []
+    second_list = []
+
+    for num in nums:
+        if current_sum + num <= target_sum:
+            first_list.append(num)
+            current_sum += num
+        else:
+            second_list.append(num)
+
+    return True
+
+
+if __name__ == '__main__':
+     a = [2, 6, 10, 2]
+     b = [1, 2, 8, 10]
+     c = [2, 2, 4]
+     print(split_list(a))
+     print(split_list(b))
+     print(split_list(c))
