eb888a703bf78f26aff88ac7f1d8b2c19202a09e
Dex1le <119615676+Dex1le@users.noreply.github.com>
2024-02-17 18:10:44 +0300

    сортировка

--- Сем 1/Lab1/main.py
+++ Сем 1/Lab1/main.py
@@ -14,7 +14,7 @@ def find_expression(numbers, target_sum, current_sum, expression, first_num):
     find_expression(numbers[1:], target_sum, current_sum - number, expression + "-" + str(number), first_num)
 
 # Считываем входные данные из файла
-with open('/Users/dex1le/PycharmProjects/university/Lab1/input.txt') as file:
+with open('/Сем 1/Lab1/input.txt') as file:
     input_data = file.readline().split()
     n = int(input_data[0])
     numbers = [int(input_data[i]) for i in range(1, n + 1)]
@@ -23,5 +23,5 @@ with open('/Users/dex1le/PycharmProjects/university/Lab1/input.txt') as file:
     first_num = numbers.pop(0)
 
 # Запускаем функцию для поиска решения
-with open('/Users/dex1le/PycharmProjects/university/Lab1/output.txt', 'w') as file:
+with open('/Сем 1/Lab1/output.txt', 'w') as file:
     file.write(find_expression(numbers, target_sum, first_num, "", first_num))
